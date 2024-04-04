3faa010c7c9ba70368f55eb73bbffe49be06f909
Gustavo M Nogueira <gustavo.mnogueira95@gmail.com>
2024-04-03 21:35:45 -0300

    server-node

---
+++ prisma/seed.ts
@@ -0,0 +1,22 @@
+import { prisma } from '../src/lib/prisma';
+
+async function seed() {
+  const eventId = '9e9bd979-9d10-4915-b339-3786b1634f33';
+
+  await prisma.event.deleteMany();
+
+  await prisma.event.create({
+    data: {
+      id: eventId,
+      title: 'Unite Summit',
+      slug: 'unite-summit',
+      details: 'Um evento p/ devs apaixonados(as) por código!',
+      maximumAttendees: 120,
+    },
+  });
+}
+
+seed().then(() => {
+  console.log('Database seeded!');
+  prisma.$disconnect();
+});
---
+++ src/utils/generate.ts
@@ -0,0 +1,11 @@
+/* eslint-disable no-useless-escape */
+/* eslint-disable prettier/prettier */
+
+export function generateSlug(text: string): string {
+  return text
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase()
+    .replace(/[^\w\s-]/g, "")
+    .replace(/\s+/g, "-");
+}
